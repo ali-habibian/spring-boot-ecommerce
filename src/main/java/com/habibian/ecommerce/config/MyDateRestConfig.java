59ee6bf0939ffc710a54803cbaada98f47aca2fd
Ali Habibian <ali.habibian04@gmail.com>
2022-02-07 18:03:08 +0330

    Added MyDateRestConfig Configure for Read Only

---
+++ src/main/java/com/habibian/ecommerce/config/MyDateRestConfig.java
@@ -0,0 +1,32 @@
+package com.habibian.ecommerce.config;
+
+import com.habibian.ecommerce.entity.Product;
+import com.habibian.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+public class MyDateRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {PUT, POST, DELETE};
+
+        // disable HTTP methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for ProductCategory: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
